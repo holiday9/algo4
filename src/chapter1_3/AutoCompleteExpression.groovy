f47df7b071caa17838461b96a2823a9bf3a04f92
yuan <htyuan@zhiniuxue.com>
2018-08-25 15:28:05 +0800

    complete 1.3.9

---
+++ src/chapter1_3/AutoCompleteExpression.groovy
@@ -0,0 +1,41 @@
+package chapter1_3
+
+class AutoCompleteExpression {
+    String autoComplete(String expressionMissLeftParenthesis) {
+        if (expressionMissLeftParenthesis == null || expressionMissLeftParenthesis == "") {
+            return null;
+        }
+
+        Stack<Character> ops = new Stack();
+        Stack<String> vals = new Stack();
+
+        for (int i = 0;i < expressionMissLeftParenthesis.length();i++) {
+            char ch = expressionMissLeftParenthesis.charAt(i);
+
+            // if ch is value, then push to vals stack
+            // if ch is (, then ignore
+            // if ch is + - * /, then push to ops stack
+            // if ch is ), then pop two value and pop a op and make a string with (), then push the result to vals
+            if (Character.isDigit(ch)) {
+                vals.push("" + ch);
+            } else if (ch == '(') {
+                // ignore
+            }
+            else if (ch == '+') ops.push(ch);
+            else if (ch == '-') ops.push(ch);
+            else if (ch == '*') ops.push(ch);
+            else if (ch == '/') ops.push(ch);
+            else if (ch == ')') {
+                String right = vals.pop();
+                String left = vals.pop();
+                String op = ops.pop();
+
+                String result = "(" + left + op + right + ")";
+
+                vals.push(result);
+            }
+        }
+
+        return vals.pop();
+    }
+}
---
+++ src/chapter1_3/Exerience1_3_9.groovy
@@ -0,0 +1,14 @@
+package chapter1_3
+
+import edu.princeton.cs.algs4.StdIn
+
+class Exerience1_3_9 {
+
+    static void main(String[] args) {
+        String expressionMissLeftParenthesis = StdIn.readString();
+
+        String expression = new AutoCompleteExpression().autoComplete(expressionMissLeftParenthesis);
+
+        printf "补全后的表达式:" + expression;
+    }
+}

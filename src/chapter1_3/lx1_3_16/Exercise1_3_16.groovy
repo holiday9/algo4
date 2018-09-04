012d8e294f4bfb623fc369cefd81078dc83e6763
yuan <htyuan@zhiniuxue.com>
2018-09-04 21:01:43 +0800

    尝试readAllInts

---
+++ src/chapter1_3/lx1_3_16/Exercise1_3_16.groovy
@@ -0,0 +1,13 @@
+package chapter1_3.lx1_3_16
+
+import edu.princeton.cs.algs4.StdIn
+
+class Exercise1_3_16 {
+    static void main(String []args) {
+        int[] ints = StdIn.readAllInts();
+
+        for (int i = 0; i < ints.length; i++) {
+            println(ints[i])
+        }
+    }
+}
---
+++ src/lx1_3_12/Exercise1_3_12.groovy
@@ -0,0 +1,8 @@
+package lx1_3_12
+
+class Exercise1_3_12 {
+    static void main(String[] args) {
+        Stack<String> stringStack = new Stack<>();
+        stringStack.iterator()
+    }
+}
